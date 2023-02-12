a3641dbf18b2b4caba38c294d62f5304fa87a4d7
pycodeorg <pradeepyadav.agt@gmail.com>
2023-02-13 02:22:53 +0530

    add two number static and dynamic

---
+++ js/custom-1.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b=30;
+let c=a+b;
+console.log("Ther sum of a and b is:",c);
+
+
+// the the value form the user
+let d=prompt("please enter first number : ","");
+let e=prompt("please enter second number : ");
+let g = parseInt(d)+parseInt(e);
+console.log("the sum of d and e is : ",g);
