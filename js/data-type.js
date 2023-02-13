0507058c995777bebfea9a430fa156fbbbb32ab1
pycodeorg <pradeepyadav.agt@gmail.com>
2023-02-14 00:03:19 +0530

    datatype in javascript

---
+++ js/data-type.js
@@ -0,0 +1,17 @@
+let a=30; //number datatype
+let name= "Pradeep"; // string datatype
+let b=true;
+
+console.log("the value of a is: ",a);
+console.log("the type of b is : ",typeof(b));
+console.log("the type of a is : ",typeof(a));//to check the datatype of variable typeof() function.
+
+
+//javascript type conversion 
+//1.number() 2.string() 3.boolean();
+
+let p= 10;
+p=String(p);
+p=p+1;
+console.log("the value of p is: " ,p);
+console.log("the type of p is: " ,typeof(p));
