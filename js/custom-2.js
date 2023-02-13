178b946f9eadef8de16e56c84431b702c9cdc084
pycodeorg <pradeepyadav.agt@gmail.com>
2023-02-14 00:32:00 +0530

    javascript debugging function and command

---
+++ js/custom-2.js
@@ -0,0 +1,7 @@
+// alert("hello world");
+// confirm("hello world");
+let a =30;
+console.log(a);
+console.log("hello Pradeep");
+// prompt("enter the number"); for user input
+document.write("<h2>Hello pradeep</h2>");
